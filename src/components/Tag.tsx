257640ff442ecf30487eb3ee1f4bd6b5c32be626
Alessio Marinelli <defkon1@gmail.com>
2023-01-30 14:13:41 +0100

    Update Tag.tsx

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -2,20 +2,25 @@ import React from 'react';
 import { Box, Text } from 'grommet';
 import { Tag as TagIcon } from 'grommet-icons';
 
-const renderTag = (value: string) => (
-  <Box
-    background="brand"
-    direction="row"
-    align="center"
-    round="xsmall"
-    pad="xsmall"
-    gap="xsmall"
-    margin="xxsmall"
-  >
-    <TagIcon color="text" style={{ width: '12px', height: '12px' }} />
-    <Text size="small"><a href="https://www.amazon.it/?k={encodeUri(value)}&tag={process.env.AMAZON_TAG}">{value}</a></Text>
-  </Box>
-);
+const renderTag = (value: string) => {
+  
+  const tagLink = "https://www.amazon.it/?k=" + encodeUri(value) + "&tag=" + process.env.AMAZON_TAG;
+  
+    return (
+    <Box
+      background="brand"
+      direction="row"
+      align="center"
+      round="xsmall"
+      pad="xsmall"
+      gap="xsmall"
+      margin="xxsmall"
+    >
+      <TagIcon color="text" style={{ width: '12px', height: '12px' }} />
+      <Text size="small"><a href={tagLink}>{value}</a></Text>
+    </Box>
+  );
+};
 
 const Tag = ({ children, ...props }: { children: string }) =>
   renderTag(children);
